91efd2aa3bf021245371ae3392bce4836bd5a1d2
pranati243 <pranati.arun@gmail.com>
2025-11-09 16:38:09 +0530

    final changes

--- frontend/config.js
+++ frontend/config.js
@@ -2,5 +2,5 @@
 // Change this to your backend URL
 // Export configuration
 window.APP_CONFIG = {
-    API_BASE_URL: 'http://localhost:5000/api'
+    API_BASE_URL: 'http://13.60.181.143:5000/api'
 };
